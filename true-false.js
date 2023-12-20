c57c23317c6ba72abefd0ece9a0d52937f0d41e7
shajibulhasan <shajibulhasan.work@gmail.com>
2023-12-20 23:03:45 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,25 @@
+//Truthy value
+// "0"
+// " "
+const name = "Soaib";
+//kono string takle true otherwise false
+if (name) {
+    console.log("Condition is true");
+} else {
+    console.log("Condition is false");
+}
+
+
+//falsy value
+// 0
+// ""
+// undefined 
+// null
+// NaN
+// false
+let name2;
+if (name2) {
+    console.log("Condition is true");
+} else {
+    console.log("Condition is false");
+}
