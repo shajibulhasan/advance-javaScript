c1abeb82631927207c1e078c8b801ade1fcb5573
shajibulhasan <shajibulhasan.work@gmail.com>
2023-12-22 21:15:11 +0600

    double vs triple equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,31 @@
+const first = 2;
+const second = "2";
+//only check value
+if(first == second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+
+const first1 = 2;
+const second2 = "2";
+// check value & type
+if(first1 === second2){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+
+const first3 = 0;
+const second3 = false;
+//only check value
+if(first3 == second3){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
